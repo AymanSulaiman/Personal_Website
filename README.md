7c7e44a7b008f3487020d1643236f5899a278a43
AymanSulaiman <saymansulaiman@gmail.com>
2020-08-10 19:09:35 -0700

    added a website and made a hello world flask app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Personal_Website
+
+This is the website about me and has my portfolios that I will be updating on a regular basis.  This will be connected to a medium blog and will have contact information
---
+++ app.py
@@ -0,0 +1,17 @@
+####STANDARD CODE
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+#########
+
+# create route that renders index.html template
+@app.route("/")
+def hello():
+    return 'hello world'
+
+####STANDARD CODE
+if __name__ == "__main__":
+    app.run(debug=True)
+#########
