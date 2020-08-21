aa9d61264fd36a67b6cde5eda8dbad06ed2cb4c9
AymanSulaiman <saymansulaiman@gmail.com>
2020-08-20 19:59:48 -0700

    started cofiguring the backend

--- app.py
+++ app.py
@@ -8,8 +8,40 @@ app = Flask(__name__)
 
 # create route that renders index.html template
 @app.route("/")
-def hello():
-    return 'hello world'
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
+@app.route("/portfolio-work")
+def portfolio_work():
+    return render_template('portfolio-work.html')
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
+
+@app.route("/single-blog")
+def single_blog():
+    return render_template('single-blog.html')
+
+@app.route("/work_details")
+def work_details():
+    return render_template('work_details.html')
 
 ####STANDARD CODE
 if __name__ == "__main__":
