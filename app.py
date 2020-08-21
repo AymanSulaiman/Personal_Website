c1b0c166c732bef654c74445d11029f00eb99498
AymanSulaiman <saymansulaiman@gmail.com>
2020-08-20 21:43:19 -0700

    changed the website, this is so much easier

--- app.py
+++ app.py
@@ -11,38 +11,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/blog")
-def blog():
-    return render_template('blog.html')
-
-@app.route("/contact")
-def contact():
-    return render_template('contact.html')
-
-@app.route("/main")
-def main():
-    return render_template('main.html')
-
-@app.route("/portfolio-work")
-def portfolio_work():
-    return render_template('portfolio-work.html')
-
-@app.route("/services")
-def services():
-    return render_template('services.html')
-
-@app.route("/single-blog")
-def single_blog():
-    return render_template('single-blog.html')
-
-@app.route("/work_details")
-def work_details():
-    return render_template('work_details.html')
-
 ####STANDARD CODE
 if __name__ == "__main__":
     app.run(debug=True)
